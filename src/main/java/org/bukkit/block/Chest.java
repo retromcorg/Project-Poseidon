ff66a694c81782b72267def301d83eaf5975d232
zavdav <157126752+zavdav@users.noreply.github.com>
2025-04-26 13:20:47 +0200

    Make Chest and Dispenser extend InventoryHolder

--- src/main/java/org/bukkit/block/Chest.java
+++ src/main/java/org/bukkit/block/Chest.java
@@ -1,8 +1,10 @@
 package org.bukkit.block;
 
+import org.bukkit.inventory.InventoryHolder;
+
 /**
  * Represents a chest.
  *
  * @author sk89q
  */
-public interface Chest extends BlockState, ContainerBlock {}
+public interface Chest extends BlockState, ContainerBlock, InventoryHolder {} // Poseidon - Backport modern Inventory API
