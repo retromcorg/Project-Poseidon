4935a0915207c333a385b508163de6de7fe90dff
Garsooon <carson12345678910@hotmail.com>
2026-06-28 01:39:30 +0900

    Add weather disable

--- src/main/java/com/legacyminecraft/poseidon/PoseidonConfig.java
+++ src/main/java/com/legacyminecraft/poseidon/PoseidonConfig.java
@@ -180,6 +180,9 @@ public class PoseidonConfig extends Configuration {
                 "This setting controls the maximum number of entities of a mob spawner type that can exist within the defined chunk radius around a mob spawner. If the number of entities exceeds this limit, the spawner will stop spawning additional entities of that type. This is useful to stop the extreme lag that can be caused by mob spawners.");
 
 
+        generateConfigOption("world.settings.disable-natural-weather-change.enabled", false);
+        generateConfigOption("world.settings.disable-natural-weather-change.info", "When enabled, weather will no longer change naturally over time. Weather can still be changed by plugins and commands.");
+
         //generateConfigOption("world-settings.eject-from-vehicle-on-teleport.enabled", true);
         //generateConfigOption("world-settings.eject-from-vehicle-on-teleport.info", "Eject the player from a boat or minecart before teleporting them preventing cross world coordinate exploits.");
 
